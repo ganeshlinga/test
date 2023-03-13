8dd0218bbf0fd49434d815fe2cefa26637134b11
ganeshlinga <ganeshlingac@gmail.com>
2023-03-13 10:38:13 -0400

    first commit1833

--- src/main/java/com/springdemo/helloworld/HelloWorldApplication.java
+++ src/main/java/com/springdemo/helloworld/HelloWorldApplication.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloWorldApplication {
 
+
     @GetMapping("/Welcome")
     public String Welcome() {
         return "HelloWorldApplication";
